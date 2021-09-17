def804f9fe148e56edc03f837036ef0ea6bbd4c2
hyooo-o <xozo5939@gmail.com>
2021-09-17 23:59:57 +0900

    string -ing2

--- baekjoon/7_5_1157.py
+++ baekjoon/7_5_1157.py
@@ -1,12 +1,12 @@
-s = input().lower()
-s_list = list(set(s))
+s = input().lower()         # 소문자
+s_list = list(set(s))       # 문자열 중복제거
 cnt_list = []
 
-for i in s_list:
-    cnt = s.count(i)
-    cnt_list.append(cnt)
+for i in s_list:            # 중복제거한 문자열이 리스트 s에서
+    cnt = s.count(i)        # 각각 몇 번 나오는지 count
+    cnt_list.append(cnt)    # 각 문자의 count 값 리스트에 따로 저장 -> 가장 많이 사용된 문자 찾기 위해
 
-if cnt_list.count(max(cnt_list)) > 1:
+if cnt_list.count(max(cnt_list)) > 1:   # 가장 많이 사용된 문자가 2개 이상일 때
     print("?")
 else:
-    print(s_list[(cnt_list.index(max(cnt_list)))].upper())
+    print(s_list[(cnt_list.index(max(cnt_list)))].upper())      # 가장 많이 사용된 문자의 인덱스 값 = 중복제거한 리스트의 인덱스 값 -> 해당 문자열 대문자로 출력
---
+++ baekjoon/7_6_1152_2.py
@@ -0,0 +1,8 @@
+''' 
+strip([문자열])
+: '문자열'에 작성한 문자를 문자열의 왼, 오에서 제거 
+'''
+
+a = input()
+a = a.strip()
+print(len(a.split(' ')))
